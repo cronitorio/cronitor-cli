d055447a43f925316136b9c6dfcbee4a38601f9a
Shane Harter <shane@cronitor.io>
2020-03-03 16:43:47 -0800

    Allow individual tests to be run

---
+++ tests/setup.sh
@@ -0,0 +1,15 @@
+CLI_LOGFILE="/tmp/test-build.log"
+CLI_LOGFILE_ALTERNATE="/tmp/test-build-alternate.log"
+CLI_CONFIGFILE="/etc/cronitor/cronitor.json"
+CLI_CONFIGFILE_ALTERNATE="/tmp/test-build-config.json"
+CLI_ACTUAL_API_KEY="cb54ac4fd16142469f2d84fc1bbebd84"
+CLI_CRONTAB_TEMP="/tmp/crontab"
+
+if [ "$1" = "--use-dev" ]
+    then
+        CRONITOR_ARGS="--use-dev"
+        HOSTNAME="http://localhost:8000"
+    else
+        CRONITOR_ARGS=""
+        HOSTNAME="https://cronitor.link"
+fi
