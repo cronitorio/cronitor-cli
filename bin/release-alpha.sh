a5e2b681c89ff8fd5803551206d7ce9674122bd1
Shane Harter <shane@cronitor.io>
2020-10-28 19:50:33 -0400

    Fix: Add Sentry DSN before creating build

--- bin/release-alpha.sh
+++ bin/release-alpha.sh
@@ -1,5 +1,6 @@
 SCRIPT_DIR=$( cd $(dirname $0) ; pwd -P )
 cd $SCRIPT_DIR
+cd ../
 
 if [ -z "$1" ]
   then echo "Usage: release-alpha.sh <version number>" && exit 1
@@ -12,11 +13,21 @@ fi
 git tag $1
 git push --tags
 
+if [ "$CRONITORCLI_SENTRY_DSN" ]
+  then echo "Adding Sentry to Build..."
+  SENTRY_DSN_ESCAPED=$(printf '%s\n' "$CRONITORCLI_SENTRY_DSN" | sed -e 's/[\/&]/\\&/g')
+  perl -pi -e "s/\/\/\ SetDSN/raven.SetDSN(\"${SENTRY_DSN_ESCAPED}\")/g" main.go
+fi
+
 equinox release \
  --version="$1" \
  --platforms="darwin_amd64 linux_amd64 linux_386 windows_amd64" \
- --signing-key=../equinox.key \
+ --signing-key=equinox.key \
  --app="app_itoJoCoW8dr" \
  --token=$CRONITORCLI_EQUINOX_TOKEN \
  --channel="alpha" \
 cronitor
+
+if [ "$CRONITORCLI_SENTRY_DSN" ]
+  then perl -pi -e "s/raven\.SetDSN\(\"${SENTRY_DSN_ESCAPED}\"\)/\/\/\ SetDSN/g" main.go
+fi
