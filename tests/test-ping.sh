8d524dc3404d7ecdb1d0112c13d1464a956a7cdc
jj <jj@jdotjdot.com>
2022-06-04 22:20:24 -0400

    ping

--- tests/test-ping.sh
+++ tests/test-ping.sh
@@ -1,57 +1,41 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bats
 
-echo "Running test-ping..."
+setup() {
+  SCRIPT_DIR="$(dirname $BATS_TEST_FILENAME)"
+  cd $SCRIPT_DIR
 
-SCRIPT_DIR=$( cd $(dirname $0) ; pwd -P )
-cd $SCRIPT_DIR
-
-source ./setup.sh
+  source $SCRIPT_DIR/setup.sh
+  rm -f $CLI_LOGFILE
+}
 
 #################
 # PING TESTS
 #################
 
-echo ""
+@test "Ping without args" {
+  ../cronitor $CRONITOR_ARGS ping d3x0c1 --run --log $CLI_LOGFILE
+  grep -q "Sending ping ${HOSTNAME}/d3x0c1/run" $CLI_LOGFILE
+}
 
-rm -f $CLI_LOGFILE
-TEST="Ping without args"
-../cronitor $CRONITOR_ARGS ping d3x0c1 --run --log $CLI_LOGFILE
-if grep -q "Sending ping ${HOSTNAME}/d3x0c1/run" $CLI_LOGFILE
-    then echo "${TEST}.. OK"
-    else echo "${TEST}.. FAIL"
-fi
+@test "Ping with custom hostname" {
+  ../cronitor $CRONITOR_ARGS ping d3x0c1 --run --hostname customHostnameForTest --log $CLI_LOGFILE
+  grep -q "Sending ping ${HOSTNAME}/d3x0c1/run" $CLI_LOGFILE && grep -q "host=customHostnameForTest" $CLI_LOGFILE
+}
 
-rm -f $CLI_LOGFILE
-TEST="Ping with custom hostname"
-../cronitor $CRONITOR_ARGS ping d3x0c1 --run --hostname customHostnameForTest --log $CLI_LOGFILE
-if grep -q "Sending ping ${HOSTNAME}/d3x0c1/run" $CLI_LOGFILE && grep -q "host=customHostnameForTest" $CLI_LOGFILE
-    then echo "${TEST}.. OK"
-    else echo "${TEST}.. FAIL"
-fi
+@test "Ping with message" {
+  MSG="messagewithoutspaces"
+  ../cronitor $CRONITOR_ARGS ping d3x0c1 --run --msg "$MSG" --log $CLI_LOGFILE
+  grep -q "Sending ping ${HOSTNAME}/d3x0c1/run" $CLI_LOGFILE && grep -q "$MSG" $CLI_LOGFILE
+}
 
-rm -f $CLI_LOGFILE
-TEST="Ping with message"
-MSG="messagewithoutspaces"
-../cronitor $CRONITOR_ARGS ping d3x0c1 --run --msg "$MSG" --log $CLI_LOGFILE
-if grep -q "Sending ping ${HOSTNAME}/d3x0c1/run" $CLI_LOGFILE && grep -q "$MSG" $CLI_LOGFILE
-    then echo "${TEST}.. OK"
-    else echo "${TEST}.. FAIL"
-fi
+@test "Ping with ping api key" {
+  KEY="XXXXXXXXXX"
+  ../cronitor $CRONITOR_ARGS ping d3x0c1 --run --ping-api-key $KEY --log $CLI_LOGFILE
+  grep -q "Sending ping" $CLI_LOGFILE && grep -q "${KEY}" $CLI_LOGFILE
+}
 
-rm -f $CLI_LOGFILE
-TEST="Ping with ping api key"
-KEY="XXXXXXXXXX"
-../cronitor $CRONITOR_ARGS ping d3x0c1 --run --ping-api-key $KEY --log $CLI_LOGFILE
-if grep -q "Sending ping" $CLI_LOGFILE && grep -q "${KEY}" $CLI_LOGFILE
-    then echo "${TEST}.. OK"
-    else echo "${TEST}.. FAIL"
-fi
-
-rm -f $CLI_LOGFILE
-TEST="Ping integration test"
-MSG=`date`
-../cronitor $CRONITOR_ARGS ping 44oI2n --run --msg "$MSG" --log $CLI_LOGFILE && sleep 1
-if ../cronitor $CRONITOR_ARGS activity 44oI2n | grep -q "$MSG"
-    then echo "${TEST}.. OK"
-    else echo "${TEST}.. FAIL"
-fi
+@test "Ping integration test" {
+  MSG=`date`
+  ../cronitor $CRONITOR_ARGS ping 44oI2n --run --msg "$MSG" --log $CLI_LOGFILE && sleep 1
+  ../cronitor $CRONITOR_ARGS activity 44oI2n | grep -q "$MSG"
+}
