29f3bbfebdb6e727b1471aa74d057dea9342724e
jj <jj@jdotjdot.com>
2022-06-20 23:32:23 -0700

    export everything

--- tests/setup_suite.bash
+++ tests/setup_suite.bash
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 setup_suite() {
-    CLI_LOGFILE="/tmp/test-build.log"
-    CLI_LOGFILE_ALTERNATE="/tmp/test-build-alternate.log"
-    CLI_CONFIGFILE="/etc/cronitor/cronitor.json"
+    export CLI_LOGFILE="/tmp/test-build.log"
+    export CLI_LOGFILE_ALTERNATE="/tmp/test-build-alternate.log"
+    export CLI_CONFIGFILE="/etc/cronitor/cronitor.json"
     #CLI_CONFIGFILE="/tmp/cronitor.json"
-    CLI_CONFIGFILE_ALTERNATE="/tmp/test-build-config.json"
+    export CLI_CONFIGFILE_ALTERNATE="/tmp/test-build-config.json"
     #CLI_ACTUAL_API_KEY="cb54ac4fd16142469f2d84fc1bbebd84"
-    CLI_ACTUAL_API_KEY="$CRONITOR_API_KEY"
-    CLI_CRONTAB_TEMP="/tmp/crontab"
-    CLI_USERNAME=`whoami`
+    export CLI_ACTUAL_API_KEY="$CRONITOR_API_KEY"
+    export CLI_CRONTAB_TEMP="/tmp/crontab"
+    export CLI_USERNAME=`whoami`
 
     if [ "$1" = "--use-dev" ]
         then
-            CRONITOR_ARGS="--use-dev"
-            HOSTNAME="http://localhost:8000"
+            export CRONITOR_ARGS="--use-dev"
+            export HOSTNAME="http://localhost:8000"
         else
-            CRONITOR_ARGS=""
-            HOSTNAME="https://cronitor.link"
+            export CRONITOR_ARGS=""
+            export HOSTNAME="https://cronitor.link"
     fi
 
-    sudo ../cronitor configure -k "$CLI_ACTUAL_API_KEY" >/dev/null 2>/dev/null
+    ../cronitor configure -k "$CLI_ACTUAL_API_KEY" >/dev/null 2>/dev/null
 }
