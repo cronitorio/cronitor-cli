11af6414348d4b28d68a01f597eb25615ced89d2
Shane Harter <shane@cronitor.io>
2023-10-21 06:27:18 -0700

    Update test-shebang.sh
    Update shebang example to match common installation paths

--- bin/test-shebang.sh
+++ bin/test-shebang.sh
@@ -1,3 +1,3 @@
-#!/Users/sharter/Dropbox/Shared/env/go/src/cronitor/cronitor -v exec d3x0c1 /usr/bin/env  cbash -e
+#!/usr/bin/cronitor -v exec d3x0c1 /usr/bin/env cbash -e
 # This script is used to test exec functionality
 echo "eureka!"
