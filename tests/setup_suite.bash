ff62d463119cba1d16344f2b82bfcb435dd7c523
Shane Harter <shane@cronitor.io>
2025-06-12 13:29:01 -0700

    Fix tests (Hopefully)

--- tests/setup_suite.bash
+++ tests/setup_suite.bash
@@ -12,9 +12,7 @@ setup_suite() {
         export CLI_CONFIGFILE="/etc/cronitor/cronitor.json"
         export CLI_CRONTAB_TEMP="$BATS_TMPDIR/crontab.txt"
     fi
-    #CLI_CONFIGFILE="/tmp/cronitor.json"
     export CLI_CONFIGFILE_ALTERNATE="$BATS_TMPDIR/test-build-config.json"
-    #CLI_ACTUAL_API_KEY="cb54ac4fd16142469f2d84fc1bbebd84"
     export CLI_ACTUAL_API_KEY="$CRONITOR_API_KEY"
     export CLI_USERNAME=`whoami`
 
@@ -27,5 +25,7 @@ setup_suite() {
             export HOSTNAME="https://cronitor.link"
     fi
 
-    ../cronitor configure --api-key "$CLI_ACTUAL_API_KEY" >/dev/null 2>/dev/null
+    # Create temp directory for initial config
+    mkdir -p "$BATS_TMPDIR"
+    CRONITOR_CONFIG="$BATS_TMPDIR/initial-config.json" ../cronitor configure --api-key "$CLI_ACTUAL_API_KEY" >/dev/null 2>/dev/null
 }
